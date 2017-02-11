ba0026a13ed4108a02d5cc2b6b1a58d953b98bc4
CaptainLevaithan <levmckinney@gmail.com>
2017-02-11 14:50:13 -0800

    first commit

---
+++ Assets/BeatAndMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatAndMusic : MonoBehaviour {
+
+    public float BPM = 120f;
+    float startOffSet = 0f;
+    float beatDuration;
+    float beatTimer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        beatDuration = 1 / (BPM / 60);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        beatTimer += Time.deltaTime;
+        
+        if(beatTimer >= beatDuration)
+        {
+            GameObject[] BeatListers = GameObject.FindGameObjectsWithTag("BeatListener");
+
+            for(int i =0; i < BeatListers.Length; ++i)
+            {
+                BeatListers[i].SendMessage("Beat", SendMessageOptions.DontRequireReceiver);
+            }
+
+            beatTimer = 0f;
+        }	
+	}
+}
