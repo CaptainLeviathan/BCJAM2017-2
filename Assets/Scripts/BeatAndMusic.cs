f6a09175465bcb267fb7a2ffe0f403526adf311f
CaptainLevaithan <levmckinney@gmail.com>
2017-02-11 20:48:01 -0800

    beat and music changes

--- Assets/Scripts/BeatAndMusic.cs
+++ Assets/Scripts/BeatAndMusic.cs
@@ -6,14 +6,22 @@ public class BeatAndMusic : MonoBehaviour
 {
 
     public float BPM = 120f;
+    public float fadeSpeed2D3D;
+
     float startOffSet = 0f;
     float beatDuration;
     float beatTimer;
+    float VolumBias = 1f;
+
+    public AudioSource player2D;
+    public AudioSource player3D;
+
 
     // Use this for initialization
     void Start()
     {
         beatDuration = 1 / (BPM / 60);
+
     }
 
     // Update is called once per frame
@@ -37,10 +45,28 @@ public class BeatAndMusic : MonoBehaviour
 
         if(Dimension.is2D())
         {
-
-        }else if(Dimension.is3D())
-        {
-
+            VolumBias += fadeSpeed2D3D * Time.deltaTime;
         }
+        else if(Dimension.is3D())
+        {
+            VolumBias -= fadeSpeed2D3D * Time.deltaTime;
+        }
+
+        fade();
+    }
+
+    void fade()
+    {
+        if (VolumBias > 1)
+        {
+            VolumBias = 1;
+        }
+        else if (VolumBias < 0)
+        {
+            VolumBias = 0;
+        }
+
+        player2D.volume = VolumBias;
+        player3D.volume = 1 - VolumBias;
     }
 }
