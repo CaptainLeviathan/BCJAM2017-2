9ac694f0739759e91869d8f3f352d6d790e9e295
CaptainLevaithan <levmckinney@gmail.com>
2017-02-12 11:54:53 -0800

    Removed Partical Pulse made changes to color changer

--- Assets/Scripts/ColorChanger.cs
+++ Assets/Scripts/ColorChanger.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class ColorChanger : MonoBehaviour
 {
     public Material material;
+    [Range(0.0f, 1.0f)]
+    public float maxR = 1f;
+    [Range(0.0f, 1.0f)]
+    public float maxG = 1f;
+    [Range(0.0f, 1.0f)]
+    public float maxB = 1f;
+    [Range(0.0f, 1.0f)]
+    public float minR = 0f;
+    [Range(0.0f, 1.0f)]
+    public float minG = 0f;
+    [Range(0.0f, 1.0f)]
+    public float minB = 0f;
 
     void Beat()
     {
         Debug.Log("beat");
-        material.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+        material.SetColor("_Color", new Color(Random.Range(minR, maxR), Random.Range(minG, maxG), Random.Range(minB, maxB)));
     }
 
 }
